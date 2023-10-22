1228db1388f836ce50f843349bbd9170a7e5e3a2
HoikanChan <hoikan.chen@gmail.com>
2023-10-22 22:00:34 +0800

    pass 2nd stage

--- cmd/mybittorrent/main_test.go
+++ cmd/mybittorrent/main_test.go
@@ -6,36 +6,34 @@ import (
 )
 
 func TestDecodeBencode(t *testing.T) {
-	// Positive test case: valid bencoded string
-	bencodedString := "4:test"
-	expected := "test"
-	result, err := decodeBencode(bencodedString)
-	if err != nil {
-		t.Errorf("Expected nil error, got %v", err)
-	}
-	if result != expected {
-		t.Errorf("Expected %s, got %s", expected, result)
+	testCases := []struct {
+		input    string
+		expected interface{}
+		err      error
+	}{
+		// Positive test cases
+		{input: "5:hello", expected: "hello", err: nil},
+		{input: "i123e", expected: "123", err: nil},
+		{input: "i-52e", expected: "-52", err: nil},
+		{input: "4:test", expected: "test", err: nil},
+
+		// Negative test cases
+		{input: "invalid", expected: "", err: fmt.Errorf("only strings are supported at the moment")},
+		{input: "l123e", expected: "", err: fmt.Errorf("only strings are supported at the moment")},
 	}
 
-	// Positive test case: valid bencoded number
-	bencodedString = "i52e"
-	expected = "52"
-	result, err = decodeBencode(bencodedString)
-	if err != nil {
-		t.Errorf("Expected nil error, got %v", err)
-	}
-	if result != expected {
-		t.Errorf("Expected %s, got %s", expected, result)
-	}
-
-	// Negative test case: invalid bencoded string
-	bencodedString = "invalid"
-	expectedErr := fmt.Errorf("only strings are supported at the moment")
-	result, err = decodeBencode(bencodedString)
-	if err == nil {
-		t.Errorf("Expected error, got nil")
-	}
-	if err.Error() != expectedErr.Error() {
-		t.Errorf("Expected error %v, got %v", expectedErr, err)
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := decodeBencode(tc.input)
+			if err != nil {
+				if tc.err == nil {
+					t.Errorf("Unexpected error: %v", err)
+				} else if err.Error() != tc.err.Error() {
+					t.Errorf("Expected error %v, but got %v", tc.err, err)
+				}
+			} else if result != tc.expected {
+				t.Errorf("Expected %v, but got %v", tc.expected, result)
+			}
+		})
 	}
 }
